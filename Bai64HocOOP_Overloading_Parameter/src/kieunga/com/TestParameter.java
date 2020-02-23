2ace30abe0c583caf99ef898f06eef8838a3f945
KieuNga <KieuNga@kieunga-is>
2020-02-23 16:52:47 +0700

    bai 64

---
+++ Bai64HocOOP_Overloading_Parameter/src/kieunga/com/TestParameter.java
@@ -0,0 +1,25 @@
+package kieunga.com;
+
+public class TestParameter {
+	public static int sum(int ... arr)
+	{
+		int s=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			s+=arr[i];
+		}
+		return s;
+	}
+
+	public static void main(String[] args) {
+		int s=sum(1,2,3,4,5);
+		System.out.println(s);
+		int s2=sum();
+		System.out.println(s2);
+		int s3=sum(7,8);
+		System.out.println(s3);
+
+
+	}
+
+}
